e890b64b21e327bf55b181d718a694d6ade2b83d
COLLINS TONUI <tckiprotich@gmail.com>
2024-11-09 09:10:37 +0000

    admin

--- revenue/middleware.ts
+++ revenue/middleware.ts
@@ -5,7 +5,7 @@ export default clerkMiddleware();
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/pay', "/admin", "/admin/categories", "/admin/collections", 
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
